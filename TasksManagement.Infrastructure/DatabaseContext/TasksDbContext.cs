1a9c025e4ba7f64ed5f9b41578e81738a6f09520
Oleksandr Kuzmeniuk <oleksandrkuzmenuk2@gmail.com>
2024-08-31 16:57:12 +0300

    Created DatabaseContext

---
+++ TasksManagement.Infrastructure/DatabaseContext/TasksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TasksManagement.Core.Entities;
+
+namespace TasksManagement.Infrastructure.DatabaseContext
+{
+	public class TasksDbContext(DbContextOptions options) : DbContext(options)
+	{
+		public DbSet<TaskTicket> TaskTickets { get; set; }
+		public DbSet<Person> People { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<TaskTicket>()
+				.Property(ticket => ticket.Status)
+				.HasConversion<string>();
+		}
+	}
+}
