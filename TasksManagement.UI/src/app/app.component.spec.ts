a15d567d9d54b1c81c0ba992294876b2d9781187
Oleksandr Kuzmeniuk <oleksandrkuzmenuk2@gmail.com>
2024-09-07 17:07:35 +0300

    Initial structure

--- TasksManagement.UI/src/app/app.component.spec.ts
+++ TasksManagement.UI/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'TaskManagement.UI' title`, () => {
+  it(`should have the 'TasksManagement.UI' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TaskManagement.UI');
+    expect(app.title).toEqual('TasksManagement.UI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TaskManagement.UI');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TasksManagement.UI');
   });
 });
---
+++ TasksManagement.UI/src/app/task-ticket/task-ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-ticket.component.html',
+  styleUrl: './task-ticket.component.css'
+})
+export class TaskTicketComponent {
+
+}
