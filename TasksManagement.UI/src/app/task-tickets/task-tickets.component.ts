88758b1adcdf819054ee6c4679a065969248b413
Oleksandr Kuzmeniuk <oleksandrkuzmenuk2@gmail.com>
2024-09-10 17:20:36 +0300

    Implemented task tickets service

--- TasksManagement.UI/src/app/task-tickets/task-tickets.component.ts
+++ TasksManagement.UI/src/app/task-tickets/task-tickets.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskTicket } from './task-ticket.model';
 import { TaskTicketComponent } from './task-ticket/task-ticket.component';
+import { UsersService } from '../user-cards/users.service';
 
 @Component({
   selector: 'app-task-tickets',
@@ -10,21 +11,16 @@ import { TaskTicketComponent } from './task-ticket/task-ticket.component';
   templateUrl: './task-tickets.component.html',
   styleUrl: './task-tickets.component.css',
 })
-export class TaskTicketsComponent {
-  taskTickets: TaskTicket[] = [
-    {
-      id: '1',
-      title: 'Task 1',
-      description: 'bbbbbb',
-      status: 'Open',
-      personId: '1',
-    },
-    {
-      id: '2',
-      title: 'Task 2',
-      description: 'aaaa',
-      status: 'Closed',
-      personId: '2',
-    },
-  ];
+export class TaskTicketsComponent implements OnInit {
+  private usersService = inject(UsersService);
+  taskTickets$ = signal<TaskTicket[]>([]);
+  error$ = signal<string | undefined>(undefined);
+  personId = '3fa85f64-5717-4562-b3fc-2c963f66afa6';
+
+  ngOnInit() {
+    this.usersService.getUser(this.personId).subscribe({
+      next: (user) => this.taskTickets$.set(user.tickets),
+      error: (error) => this.error$.set(error),
+    });
+  }
 }
--- TasksManagement.UI/src/app/task-tickets/task-tickets.service.ts
+++ TasksManagement.UI/src/app/task-tickets/task-tickets.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TaskTicket } from './task-ticket.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskTicketsService {
+  private httpClient = inject(HttpClient);
+  private apiUrl = environment.apiUrl;
 
-  constructor() { }
+  getTaskTickets(): Observable<TaskTicket[]> {
+    return this.httpClient.get<TaskTicket[]>(`${this.apiUrl}/TaskTickets`);
+  }
+
+  getTaskTicket(id: string): Observable<TaskTicket> {
+    return this.httpClient.get<TaskTicket>(`${this.apiUrl}/TaskTickets/${id}`);
+  }
+
+  putTaskTicket(taskTicket: TaskTicket): Observable<Object> {
+    return this.httpClient.post(
+      `${this.apiUrl}/TaskTickets/${taskTicket.id}`,
+      taskTicket
+    );
+  }
+
+  postTaskTicket(taskTicket: TaskTicket): Observable<Object> {
+    return this.httpClient.post(`${this.apiUrl}/TaskTickets/`, taskTicket);
+  }
+
+  deleteTaskTicket(id: string): Observable<Object> {
+    return this.httpClient.delete(`${this.apiUrl}/TaskTickets/${id}`);
+  }
 }
